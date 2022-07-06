9f5f64fa6c0f3c83eb072c44b350b4cb911d9250
Sainathan Iyer <sainathaniyer@gmail.com>
2022-07-06 10:47:33 +0530

    added entity file

--- src/main/java/com/springboot/db2hibernate/model/Employee.java
+++ src/main/java/com/springboot/db2hibernate/model/Employee.java
@@ -1,6 +1,11 @@
 package com.springboot.db2hibernate.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
@@ -8,9 +13,17 @@ import lombok.Data;
 @Entity
 @Table(name = "employees")
 public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name = "first_name", nullable = false)
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	@Column(name = "email")
 	private String email;
 
 }
